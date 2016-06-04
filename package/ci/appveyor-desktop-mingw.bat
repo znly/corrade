cc14b177ac4bbdb1cd56f382ea98d4becd4cf99a
Vladimír Vondruš <mosra@centrum.cz>
2016-06-05 01:02:46 +0200

    [wip] ninja?

--- package/ci/appveyor-desktop-mingw.bat
+++ package/ci/appveyor-desktop-mingw.bat
@@ -2,13 +2,13 @@ rem Workaround for CMake not wanting sh.exe on PATH for MinGW. AARGH.
 set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
 set PATH=C:\tools\mingw64\bin;%APPVEYOR_BUILD_FOLDER%\deps\bin;%PATH%
 
-rem Build. Could not get Ninja to work, meh.
+rem Build
 mkdir build && cd build || exit /b
 cmake .. ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
     -DBUILD_TESTS=ON ^
-    -G "MinGW Makefiles" || exit /b
+    -G Ninja || exit /b
 cmake --build . -- -j || exit /b
 cmake --build . --target install -- -j || exit /b
 
